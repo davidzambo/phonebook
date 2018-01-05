e59af120b1d9d09347863f40cd8799fab3349924
Zámbó Sándor Dávid <david@dcmf.hu>
2018-01-05 13:42:38 +0100

    PDF generation works with mock data

---
+++ src/phonebook/PdfGeneration.java
@@ -0,0 +1,75 @@
+
+package phonebook;
+
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.GrayColor;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileOutputStream;
+import javafx.collections.ObservableList;
+
+
+public class PdfGeneration {
+        void pdfGeneration(String fileName, ObservableList<Person> data){
+        Document document = new Document();
+        
+        try {
+            // céges logó
+            PdfWriter.getInstance(document, new FileOutputStream(fileName + ".pdf"));
+            document.open();
+            Image image1 = Image.getInstance(getClass().getResource("/logo.jpg"));
+            image1.scaleToFit(200, 86);
+            image1.setAbsolutePosition(200f,750f);
+            document.add(image1);
+            
+            // Táblázat
+            float[] columnWidths = {3, 3, 4};
+            PdfPTable table = new PdfPTable(columnWidths);
+            table.setWidthPercentage(100);
+            
+            PdfPCell cell = new PdfPCell(new Phrase("KontaktLista"));
+            cell.setBackgroundColor(GrayColor.GRAYWHITE);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell.setColspan(3);
+            table.addCell(cell);
+            
+            table.getDefaultCell().setBackgroundColor(new GrayColor(0.75f));
+            table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell("Vezetéknév");
+            table.addCell("Keresztnév");
+            table.addCell("E-mail cím");
+            table.setHeaderRows(1);
+            
+            table.getDefaultCell().setBackgroundColor(GrayColor.GRAYWHITE);
+            table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
+            
+            for (int counter = 1; counter < 10; counter++){
+                table.addCell(String.valueOf(counter));
+                table.addCell("key " + counter);
+                table.addCell("value " + counter);
+            }
+            
+            document.add(table);
+            
+            
+            
+            // Aláírás
+            Chunk signature = new Chunk("\n\n Generalva a telefonkönyv alkalmazás segítségével.");
+            Paragraph base = new Paragraph(signature);
+            document.add(base);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        document.close();
+    }
+
+}
---
+++ src/phonebook/ViewController.java
@@ -0,0 +1,205 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phonebook;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
+/**
+ *
+ * @author zdavid
+ */
+public class ViewController implements Initializable {
+
+    @FXML
+    TableView table;
+    @FXML
+    TextField inputLastName;
+    @FXML
+    TextField inputFirstName;
+    @FXML
+    TextField inputEmail;
+    @FXML
+    Button addNewContactButton;
+    @FXML
+    StackPane menuPane;
+    @FXML
+    Pane contactPane;
+    @FXML
+    Pane exportPane;
+    @FXML
+    TextField inputExportName;
+    @FXML
+    Button exportButton;
+
+    private final String MENU_CONTACTS = "Kontaktok";
+    private final String MENU_LIST = "Lista";
+    private final String MENU_EXPORT = "Exportálás";
+    private final String MENU_EXIT = "Kilépés";
+
+    private final ObservableList<Person> data
+            = FXCollections.observableArrayList(
+                    new Person("Szabó", "Gyula", "gyuszi.teszt@example.com"),
+                    new Person("Bourne", "Jason", "secret@citromail.hu"),
+                    new Person("Scott", "Michael", "thatswhatshesaid@example.com"));
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        setTableData();
+        setMenuData();
+
+    }
+
+    @FXML
+    private void addContact(ActionEvent event) {
+        String email = inputEmail.getText();
+        if (email.length() > 3 && email.contains("@") && email.contains(".")) {
+            data.add(new Person(inputLastName.getText(), inputFirstName.getText(), email));
+            inputLastName.clear();
+            inputFirstName.clear();
+            inputEmail.clear();
+        }
+    }
+
+    @FXML
+    private void exportList(ActionEvent event) {
+        String fileName = inputExportName.getText();
+        fileName = fileName.replaceAll("\\s+","");
+        
+        if (fileName != null && fileName != "") {
+            PdfGeneration pdfCreator = new PdfGeneration();
+            pdfCreator.pdfGeneration(fileName, data);
+        }
+    }
+
+    private void setTableData() {
+        TableColumn lastNameCol = new TableColumn("Vezetéknév");
+        lastNameCol.setMinWidth(100);
+        lastNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        lastNameCol.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
+        lastNameCol.setOnEditCommit(
+                new EventHandler<TableColumn.CellEditEvent<Person, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Person, String> t) {
+                ((Person) t.getTableView().getItems().get(
+                        t.getTablePosition().getRow())).setLastName(t.getNewValue());
+            }
+        }
+        );
+
+        TableColumn firstNameCol = new TableColumn("Keresztnév");
+        firstNameCol.setMinWidth(100);
+        firstNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        firstNameCol.setCellValueFactory(new PropertyValueFactory<Person, String>("firstName"));
+        firstNameCol.setOnEditCommit(
+                new EventHandler<TableColumn.CellEditEvent<Person, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Person, String> t) {
+                ((Person) t.getTableView().getItems().get(
+                        t.getTablePosition().getRow())).setFirstName(t.getNewValue());
+            }
+        }
+        );
+
+        TableColumn emailCol = new TableColumn("Email");
+        emailCol.setMinWidth(200);
+        emailCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        emailCol.setCellValueFactory(new PropertyValueFactory<Person, String>("email"));
+        emailCol.setOnEditCommit(
+                new EventHandler<TableColumn.CellEditEvent<Person, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Person, String> t) {
+                ((Person) t.getTableView().getItems().get(
+                        t.getTablePosition().getRow())).setEmail(t.getNewValue());
+            }
+        }
+        );
+
+        table.getColumns().addAll(lastNameCol, firstNameCol, emailCol);
+
+        table.setItems(data);
+    }
+
+    private void setMenuData() {
+        TreeItem<String> treeItemRoot1 = new TreeItem<>("Menu");
+        TreeView<String> treeView = new TreeView<>(treeItemRoot1);
+        treeView.setShowRoot(false);
+
+        TreeItem<String> nodeItemA = new TreeItem<>(MENU_CONTACTS);
+        TreeItem<String> nodeItemB = new TreeItem<>(MENU_EXIT);
+
+        nodeItemA.setExpanded(true);
+
+        Node contactsNode = new ImageView(new Image(getClass().getResourceAsStream("/contacts.png")) {
+        });
+        Node exportNode = new ImageView(new Image(getClass().getResourceAsStream("/export.png")));
+
+        TreeItem<String> nodeItemA1 = new TreeItem<>(MENU_LIST, contactsNode);
+        TreeItem<String> nodeItemA2 = new TreeItem<>(MENU_EXPORT, exportNode);
+
+        nodeItemA.getChildren().addAll(nodeItemA1, nodeItemA2);
+
+        treeItemRoot1.getChildren().addAll(nodeItemA, nodeItemB);
+
+        menuPane.getChildren().add(treeView);
+
+        treeView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                TreeItem<String> selectedItem = (TreeItem<String>) newValue;
+                String selectedMenu;
+                selectedMenu = selectedItem.getValue();
+
+                if (null != selectedMenu) {
+                    switch (selectedMenu) {
+                        case MENU_CONTACTS:
+                            selectedItem.setExpanded(true);
+                            break;
+                        case MENU_LIST:
+                            contactPane.setVisible(true);
+                            exportPane.setVisible(false);
+                            break;
+                        case MENU_EXPORT:
+                            contactPane.setVisible(false);
+                            exportPane.setVisible(true);
+                            break;
+                        case MENU_EXIT:
+                            System.exit(0);
+                            break;
+                    }
+                }
+            }
+        });
+    }
+
+}
